7c406a5f86256cab98c6158287ac15d6c42880b0
Mikenson <thomasmikenson40@gmail.com>
2023-08-20 16:16:03 -0300

    Ultima versão

--- calculadora.py
+++ calculadora.py
@@ -27,40 +27,31 @@ janela.config(bg="gray")
 janela.geometry('500x425')
 
 nome_calculadra = Label(janela, text= "Calculadora")
-nome_calculadra.grid(column = 0, row = 0)
 nome_calculadra.place(relx=.5, rely=.05, anchor="center")
 
 
 peso = Label(janela, text= "Digite seu peso")
-peso.grid(row=0, column=0)
 peso.place(relx=.3, rely=.2, anchor="center")
 
 peso_pessoa = tk.Entry(janela)
-peso_pessoa.grid(row=0 ,column=0)
 peso_pessoa.place(relx=.6, rely=.2, anchor="center")
 
 altura = Label(janela, text= "Digite sua altura")
-altura.grid(row=2, column=0)
 altura.place(relx=.3, rely=.3, anchor="center")
 
 altura_pessoa = tk.Entry(janela)
-altura_pessoa.grid(row=2, column=0)
 altura_pessoa.place(relx=.6, rely=.3, anchor="center")
 
 botao = Button(janela, text="Calcular", command=pegar_imc)
-botao.grid(row=4, column=0)
 botao.place(relx=.4, rely=.4)
 
 label_salvo = Label(janela, text="", background="gray")
-label_salvo.grid(row=6, column=2)
 label_salvo.place(relx=.5, rely=.6, anchor="center")
 
 imc = Label(janela, text= "Seu IMC é:")
-imc.grid(row=6, column=0)
 imc.place(relx=.3, rely=.6, anchor="center")
 
 msg = Label(janela, text="", background="gray")
-msg.grid(row=9, column=1)
 msg.place(relx=.5, rely=.8, anchor="center")
 
 janela.mainloop()
